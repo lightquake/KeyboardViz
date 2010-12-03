4b3f314f4c93ec154f5c9db903f9372f4a3786fb
Patrick Hurst <phurst@amateurtopologist.com>
2010-12-03 13:52:56 -0500

    make the NSTextViews unselectable

--- KeyRenderer.m
+++ KeyRenderer.m
@@ -20,6 +20,7 @@
 	[textView setFont: [NSFont systemFontOfSize: 25.0]];
 	[textView setTextColor: [NSColor whiteColor]];
 	[textView setEditable: NO];
+	[textView setSelectable: NO];
 	[self addSubview: textView];
 	return self;
 }
@@ -52,7 +53,7 @@
 }
 
 - (void) drawRect:(NSRect)dirtyRect {
-	NSColor *color = [NSColor colorWithCalibratedHue: .7-(presses * .02) saturation: 1 brightness: .2+(presses * .1) alpha: 1];
+	NSColor *color = [NSColor colorWithCalibratedHue: .7-(presses * .002) saturation: 1 brightness: .2+(presses * .01) alpha: 1];
 	[color set];
 	NSRectFill([self bounds]);
 }
